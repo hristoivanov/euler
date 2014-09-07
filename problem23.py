acd13b0549863920706fbb71a81588fb213e472c
Hristo Ivanov <hristo.idgh@gmail.com>
2014-09-07 22:38:39 +0200

    x40 Speed up for problem 23

--- problem23.py
+++ problem23.py
@@ -1,12 +1,13 @@
 import math
 def is_abundant(num):
+	# The use of a set for 'divisors' slow us.
 	divisors=[1]
 	for x in range(2, int(math.floor( math.sqrt(num)))+1):
 		if num%x==0:
 			divisors.append(x)
 			res=num/x
 			if res!=x:
-				divisors.append(num/x)
+				divisors.append(res)
 	if sum(divisors)>num:
 		return True
 	return False
@@ -35,48 +36,39 @@ abundants=calc_abundants()
 # First we discart the even numbers that cant be expresed as a sum of two abundant numbers.
 # The diference between consecutive even abundant numbers is as much 6 and most of the times 4 or 2.
 # This make easy to express most of the even numbers as a sum of two abundant numbers
-unreachable=[2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 26, 28, 34, 46]
+unreachable=set([2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 26, 28, 34, 46])
 # All the other even numbers can be expresed as a sum of two abundant numbers
 
 # We will add all the odd numbers and then we will remove the one that we arent interested in.
 odd=1
 while True:
-	unreachable.append(odd)
+	unreachable.add(odd)
 	odd+=2
-	if odd==49:
-		unreachable=sorted(unreachable)
 	if odd>28123:
 		break;
 
 
 
 # Separate the even and odd abundant numbers
-abundants_odd=[]
-abundants_even=[]
+abundants_odd=set()
+abundants_even=set()
 for num in abundants:
 	if num%2!=0:
-		abundants_odd.append(num)
+		abundants_odd.add(num)
 	else:
-		abundants_even.append(num)
+		abundants_even.add(num)
 
 
 # Calculate all the odd numbers that can be expresed as a sum of two abundant numbers
-odd_bad=[]
+odd_bad=set()
 for odd in abundants_odd:
 	for even in abundants_even:
 		the_bad=odd+even
 		if the_bad>28123:
-			continue
-		if not(the_bad in odd_bad):
-			odd_bad.append(the_bad)
-odd_bad=sorted(odd_bad)
-
-# Remove the unwanted even numbers
-the_bads=[]
-for pot_bad in unreachable:
-	if not(pot_bad in odd_bad):
-		the_bads.append(pot_bad)
+			break
+		odd_bad.add(the_bad)
+		unreachable.discard(the_bad)
 
 # Finally print the answer
-the_ans=sum(the_bads)
+the_ans= sum(unreachable)
 print 'Answer: '+`the_ans`+' Time: '+`time.time()-start`
