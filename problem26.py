3ad244611e3ecc7a13e0eed9daff61c3b962b174
Hristo Ivanov <hristo.idgh@gmail.com>
2014-09-09 11:53:22 +0200

    Faster implementation for the problem 26

--- problem26.py
+++ problem26.py
@@ -1,41 +1,23 @@
 import time
-import sys
-sys.setrecursionlimit(90000)
 start=time.time()
 
-
-def calc_digit(num, div):
-	digit=num/div
-	num=(num%div)*10
-	return digit, num
-
-def calc_division(num, div, max_digit, curr_digit=0, result=[]):
-	for x in range(0, max_digit):
-		if num==0:
-			return result
-
-		digit, num=calc_digit(num, div)
-		result.append(digit)
-
-	return result
-
-def len_cycle(digits, max_digit_len):
-	if len(digits)<max_digit_len:
-		return 0
-	for x in range(1, max_digit_len/2):
-		if digits[-x:]==digits[-x*2:-x]:
-			return x
-
-	return -1
-	print 'Error insuficient max_digit_len'
+def len_cycle2(num):
+	rest=10%num
+	list_rests=[10]
+	while True:
+		rest=(rest%num)*10
+		if rest==0:
+			return 0
+		if rest in list_rests:
+			return len(list_rests)-list_rests.index(rest)
+		list_rests.append(rest)
 
 
-max_digit_len=2000
+
 solution=0
 max_len=0
 for x in range(1, 1000):
-	digits=calc_division(1, x, max_digit_len, 0, [])
-	the_len=len_cycle(digits, max_digit_len)
+	the_len=len_cycle2(x)
 	if the_len>max_len:
 		solution=x
 		max_len=the_len
